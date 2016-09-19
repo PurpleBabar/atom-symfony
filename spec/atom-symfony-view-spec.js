943f045b58913be46d9aee77dd3d9f2bf6504ddf
PurpleBabar <lalung.alexandre@gmail.com>
2016-09-19 11:44:26 +0200

    First !

---
+++ spec/atom-symfony-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSymfonyView from '../lib/atom-symfony-view';
+
+describe('AtomSymfonyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
